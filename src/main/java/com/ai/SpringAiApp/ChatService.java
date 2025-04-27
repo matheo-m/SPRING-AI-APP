203071a92e0275768f0bd06fe366b63444422594
MatheoM35 <mousse@et.esiea.fr>
2025-04-27 23:13:51 +0200

    add constructor and response method to ChatService

--- src/main/java/com/ai/SpringAiApp/ChatService.java
+++ src/main/java/com/ai/SpringAiApp/ChatService.java
@@ -1,4 +1,15 @@
 package com.ai.SpringAiApp;
 
+import org.springframework.ai.chat.model.ChatModel;
+
 public class ChatService {
+    private final ChatModel chatModel;
+
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt){
+        return chatModel.call(prompt);
+    }
 }
